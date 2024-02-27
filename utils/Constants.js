1d10355255ba3005cad5c6c72d2aef29bb2adfc0
Waqar Alam <developer4@bizency.com>
2024-02-27 15:11:47 +0530

    Commit 1: open chat

---
+++ utils/Constants.js
@@ -0,0 +1,18 @@
+module.exports = {
+    APP_NAME: "BizChat",
+    DESCRIPTION: "Welcome to secure chat network.",
+
+    SOCAIL_LINKS: {
+        github: "https://github.com/yogeshjha06",
+        linkedin: "https://www.linkedin.com/in/yogeshjha06/",
+    },
+
+    SOCKET_EVENTS: {
+        JOIN_ROOM: "joinRoom",
+        CHAT_BOT: "chatbot",
+        CHAT_MESSAGE: "chatMessage",
+        ROOM_USERS: "roomUsers",
+        DISCONNECT: "disconnect",
+    },
+    MIN_USERNAME_LENGTH: 6,
+};
